e42b59e4f78ff7b7be50e22d58f57cc39310f8f6
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-09-12 18:12:58 -0700

    101- adjust betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,17 @@
 
 /**
  * main - Main entry point
- * Description: UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * Description: UNIX is simple
  * Return: 1
  */
 
 int main(void)
 {
 	char *words = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while (*words) putchar(*words++);
-	return 1;
+
+	while (*words)
+	{
+		putchar(*words++);
+	}
+	return (1);
 }
