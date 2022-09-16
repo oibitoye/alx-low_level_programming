1642d8f2a539a04c08ccb810ca54a82202fdf743
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-09-15 19:47:43 -0700

    main mod

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,11 @@
 #define YUSSUPH_HEAD
 
 int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void)
 
 #endif
