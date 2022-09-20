9531093fdcfda04aceff0825d1b87968391cb3f0
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-09-20 14:12:09 -0700

    task 8-9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,24 @@
 
 
 /**
- * * print_array - prints n element of array
- * @a: array
+ * print_array - prints n element of array
+ * @a: input array
  * @n: number of elements
- * Return: nothing
  */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
+
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string including null byte
+ * @src: Source string
+ * @dest: Destination buffer
+ * Return: Destination buffer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	printf("%d\n",*(src + i) != '\0');
+	*(dest + i) = '\0';
+
+	return (dest);
+}
+
