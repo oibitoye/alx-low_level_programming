3ada396a9037dfdd4363a954771520982c8624a8
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-09-19 21:57:53 -0700

    task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _strlen - get length og string
+ * @s: Input string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * puts_half - prints the last half of a string
+ * @s: Input string
+ * Return: int
+ */
+
+void puts_half(char *s)
+{
+	int len = _strlen(s);
+	int i;
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(s[i]);
+		}
+		else
+		{
+			for (i = len / 2 + 1; i < len; i++)
+			{
+				_putchar(s[i]);
+			}
+		}
+		_putchar('\n');
+}
+
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * * print_array - prints n element of array
+ * @a: array
+ * @n: number of elements
+ * Return: nothing
+ */
