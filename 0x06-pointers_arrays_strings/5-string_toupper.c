dc663dbc05f90934f7935e045c31035bc52f5215
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-09-23 21:15:22 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - function changes all lowercase letters to  uppercase
+ * @n: Input string
+ * Return: String
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+		else
+		{
+			continue;
+		}
+		i++;
+	}
+	return (n);
+}
+
