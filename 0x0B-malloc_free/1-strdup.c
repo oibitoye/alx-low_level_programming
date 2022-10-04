2f41fd83c3355e745fcc2376f55ae77be305fbf2
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-04 15:48:20 -0700

    task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - function that returns a pointer to new space
+ * @str: String to be duplicated
+ * Return: returns new pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i = 0;
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (*(str + i) != '\0')
+		{
+			len = len + i;
+			i++;
+		}
+		new_str = malloc(len * sizeof(char));
+		for (i = 0; i < len; i++)
+		{
+			new_str[i] = *(str + i);
+		}
+		return (new_str);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
