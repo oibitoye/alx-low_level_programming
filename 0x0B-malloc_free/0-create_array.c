f293322ed43046dbeca06780fb5ee2ebc213a9f2
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-04 14:36:50 -0700

    task 0 - modified

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc((size + 1) * sizeof(char));
+	char *arr = malloc(size * sizeof(char));
 	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
+	}
+	if (arr == NULL)
+	{
+		return (1);
 	}
 	for (i = 0; i < size; i++)
 	{
