dc85c2d0419d30547b5c6592f761a66a2f73ead2
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-04 14:28:21 -0700

    task 0 - modified

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,13 +6,16 @@
  * @size: Size of array
  * @c: Input characters
  * Return: array of characters
- * */
+ */
 
 char *create_array(unsigned int size, char c)
 {
 	char *arr = malloc(size * sizeof(char));
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
