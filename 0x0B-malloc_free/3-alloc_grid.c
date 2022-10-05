c9c6f9fff5d640a656c1e80172d0ebb5cd6ad340
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-05 09:33:19 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,13 +17,13 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr = malloc(height * sizeof(int));
+	arr = (int **)malloc(height * sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(width * sizeof(int));
+		arr[i] = (int *)malloc(width * sizeof(int));
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
