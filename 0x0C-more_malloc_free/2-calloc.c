3730dbbcff4d907ae2923974555d51263ecebb0e
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-06 16:26:04 -0700

    task 2 edited

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,15 +10,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *arr;
+	unsigned int i;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	arr = (unsigned int *)malloc(nmemb * size);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		arr[i] = 0;
+	}
 	return (arr);
 }
 
