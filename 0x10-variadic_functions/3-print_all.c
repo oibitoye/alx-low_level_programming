65178cafd59b5ebdb362b2935557a40fcf9fa487
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-13 22:02:49 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+
+void print_all(const char *const format, ...)
+{
+	int i = 0;
+	va_list items;
+	char *s = ", ";
+	char *p;
+
+	va_start(items, format);
+
+	while ((format != NULL) && *(format + i) != '\0')
+	{
+		if (*(format + i) == 's')
+		{
+			p = va_arg(items, char *);
+			p = (p == NULL) ? "(nil)" : p;
+			printf("%s", p)
+		}
+		else if (*(format + i) == 'i')
+		{
+			printf("%i", va_arg(items, int));
+		}
+		else if (*(format + i) == 'c')
+		{
+			printf("%c", va_arg(items, int));
+		}
+		else if (*(format + i) == 'f')
+		{
+			printf("%f", va_arg(items, double));
+		}
+		else
+		{
+			continue;
+		}
+		i++
+	}
+	putchar(10);
+	va_end(items);
+}
