09d4dc025b43277e76f1ff81d6f8a69cc7e7346b
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-28 20:45:26 -0700

    Task 3 modified

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,11 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 
-	if (index > 63)
+	if (!n || (index > (sizeof(unsigned long int) * 8) - 1))
 		return (-1);
 
-	*n = (*n | (1 << index));
+	*n = *n | (1 << index);
 
 	return (1);
 }
+
