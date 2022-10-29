f24d462a713ebd3b33359956155c98297b0ad696
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2022-10-28 20:34:56 -0700

    Task 1 modified

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,10 +12,11 @@ void print_binary(unsigned long int n)
 	{
 		_putchar(48);
 	}
-
-	if (n >> 1)
-		print_binary(n >> 1);
-
-	_putchar((n & 1) + 48);
+	else
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + 48);
+	}
 }
 
